b76de8edd883de48297034d99aca249309e11251
Pirush Prechathavanich <pirush@nimbl3.com>
2018-06-14 14:29:47 +0700

    Init project and add git ignore

---
+++ Nimbus/Nimbus/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Nimbus
+//
+//  Created by Pirush Prechathavanich on 6/14/18.
+//  Copyright © 2018 Nimbl3. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Nimbus/Nimbus/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Nimbus
+//
+//  Created by Pirush Prechathavanich on 6/14/18.
+//  Copyright © 2018 Nimbl3. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
