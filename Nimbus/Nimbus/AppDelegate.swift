ed71ad279bc5d3c8d842cb3a6259ea8d4ec974c3
Pirush Prechathavanich <pirush@nimbl3.com>
2018-06-14 15:10:59 +0700

    Add app icon and set up status item

--- Nimbus/Nimbus/AppDelegate.swift
+++ Nimbus/Nimbus/AppDelegate.swift
@@ -7,20 +7,49 @@
 //
 
 import Cocoa
+import Foundation
+import AppKit
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
-
-
+    
+    private let statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.squareLength)
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to initialize your application
+        setupIcon()
+        setupMenu()
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
     }
+    
+    // MARK: - private helper
+    
+    private func setupIcon() {
+        guard let button = statusItem.button else { return }
+        button.image = #imageLiteral(resourceName: "icon.post-it")
+        button.action = #selector(click)
+    }
 
+    private func setupMenu() {
+        let menu = NSMenu()
+        
+        menu.addItem(NSMenuItem(title: "First", action: #selector(printSomething), keyEquivalent: "1"))
+        menu.addItem(NSMenuItem(title: "Second", action: #selector(printSomething), keyEquivalent: "2"))
+        menu.addItem(NSMenuItem.separator())
+        menu.addItem(NSMenuItem(title: "Quit", action: #selector(NSApplication.terminate), keyEquivalent: "q"))
+        
+        statusItem.menu = menu
+    }
+    
+    @objc private func click() {
+        print("### click")
+    }
+    
+    @objc private func printSomething(_ sender: NSMenuItem) {
+        print("### item: \(sender.title)")
+    }
 
 }
 
