fcc0cb09b4b235c9f1a640c7096ba80b944af53d
Pirush Prechathavanich <pirush@nimbl3.com>
2018-06-15 15:50:26 +0700

    Create HotkeyManager to handle copy story id to clipboard

--- Nimbus/Nimbus/Controllers/ApplicationController.swift
+++ Nimbus/Nimbus/Controllers/ApplicationController.swift
@@ -16,29 +16,25 @@ final class ApplicationController {
     private let credentialsStorage = CredentialsStorage()
     private let manager: RequestManager
     
-    private lazy var popover: NSPopover = {
-        let popover = NSPopover()
-        popover.behavior = .transient
-        return popover
-    }()
-    
     // MARK: - controllers
     
     private let accountController: AccountMenuController
     private let projectController: ProjectController
     private let storiesController: StoriesController
     
+    private let hotkeyManager = HotkeyManager()
+    
     // MARK: - menu items
     
     private let statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.squareLength)
     
     private let fetchMenuItem = NSMenuItem(title: "Fetch", action: nil, keyEquivalent: "F")
+    private var quitMenuItem = NSMenuItem(
+        title: "Quit",
+        action: #selector(NSApplication.terminate),
+        keyEquivalent: "q"
+    )
     
-    private let projectsMenuItem = NSMenuItem(title: "Select project", action: nil, keyEquivalent: "")
-    
-    private var quitMenuItem = NSMenuItem(title: "Quit",
-                                          action: #selector(NSApplication.terminate),
-                                          keyEquivalent: "q")
     init() {
         let adapter = TokenAdapter(credentialsProvider: credentialsStorage)
         manager = RequestManager(adapter: adapter)
@@ -78,8 +74,12 @@ final class ApplicationController {
             menu.addItem(.separator())
             
             storiesController.items.forEach(menu.addItem)
-            storiesController.onUpdateStories = { [weak self] _ in self?.setupMenu(with: account) }
-            storiesController.onSelectStory = nil
+            storiesController.onUpdateStories = { [weak self] _ in
+                self?.setupMenu(with: account)
+            }
+            storiesController.onSelectStory = { [weak self] story in
+                self?.hotkeyManager.configureCopyHotkey(for: story)
+            }
             
             menu.addItem(.separator())
         }
@@ -93,92 +93,4 @@ final class ApplicationController {
         menu.addItem(quitMenuItem)
     }
     
-    // MARK: - private helper
-    
-    private func fetchStories(of project: Project) {
-        let request = Requests.PivotalTracker.stories(ofProjectId: project.projectId,
-                                                      withState: .started)
-        manager.perform(request)
-            .startWithResult { [weak self] result in
-                switch result {
-                case .success(let stories):
-                    self?.stories = stories
-                    self?.updateStoriesMenu(of: project, with: stories)
-                case .failure(let error):
-                    debugPrint("### error", error)
-                }
-        }
-    }
-    
-    private let projectTitleMenuItem = NSMenuItem(title: "No project selected", action: nil, keyEquivalent: "")
-    private var selectedStoryItem: NSMenuItem?
-    
-    
-    private func updateStoriesMenu(of project: Project, with stories: [Story]) {
-        guard
-            let menu = statusItem.menu,
-            let index = menu.items.index(of: projectTitleMenuItem)
-        else { return }
-        projectTitleMenuItem.title = project.projectName
-        stories.enumerated().forEach {
-            let item = createItem(title: $1.name, action: #selector(focusStory))
-            menu.insertItem(item, at: (index + 1) + $0)
-        }
-    }
-    
-    @objc private func focusStory(_ item: NSMenuItem) {
-        guard let story = stories.first(where: { $0.name == item.title }) else { return }
-
-        selectedStory = story
-        selectedStoryItem?.state = .off
-        selectedStoryItem = item
-        
-        item.state = .on
-        storyHotkey.keyDownHandler = {
-            let pasteboard = NSPasteboard.general
-            pasteboard.clearContents()
-            pasteboard.setString("\(story.id)", forType: .string)
-            
-            let notification = NSUserNotification()
-            notification.title = story.name
-            notification.informativeText = "The story's ID has been copied to your clipboard"
-            notification.soundName = NSUserNotificationDefaultSoundName
-            NSUserNotificationCenter.default.deliver(notification)
-        }
-    }
-    
-    private func updateMenu(with account: Account) {
-        guard let menu = statusItem.menu else { return }
-        menu.removeAllItems()
-        
-        menu.insertItem(fetchMenuItem, at: 0)
-        
-        menu.addItem(projectsMenuItem)
-        
-        menu.addItem(NSMenuItem.separator())
-        menu.addItem(projectTitleMenuItem)
-        menu.addItem(NSMenuItem.separator())
-        
-        menu.addItem(quitMenuItem)
-    }
-    
-    // MARK: - private configuration
-    
-    private var stories: [Story] = []
-    private var selectedStory: Story?
-    
-    private var projects: [Project] = []
-    private var selectedProject: Project? {
-        didSet {
-            guard let project = selectedProject else { return }
-            fetchStories(of: project)
-        }
-    }
-    
-    private func createItem(title: String, action: Selector?, key: String = "") -> NSMenuItem {
-        let item = NSMenuItem(title: title, action: action, keyEquivalent: key)
-        item.target = self
-        return item
-    }
-
 }
---
+++ Nimbus/Nimbus/Controllers/CopyManager.swift
@@ -0,0 +1,35 @@
+//
+//  CopyManager.swift
+//  Nimbus
+//
+//  Created by Pirush Prechathavanich on 6/15/18.
+//  Copyright © 2018 Nimbl3. All rights reserved.
+//
+
+import Cocoa
+import HotKey
+
+final class HotkeyManager {
+    
+    private let copyHotkey = HotKey(key: .c, modifiers: [.command, .shift])
+    
+    func configureCopyHotkey(for story: Story, withNotification: Bool = true) {
+        copyHotkey.keyDownHandler = { [weak self] in
+            self?.copyToPasteboard("\(story.id)")
+            self?.notify(with: story)
+        }
+    }
+    
+    private func copyToPasteboard(_ text: String) {
+        let pasteboard = NSPasteboard.general
+        pasteboard.clearContents()
+        pasteboard.setString(text, forType: .string)
+    }
+    
+    private func notify(with story: Story) {
+        let notification = NSUserNotification()
+        notification.title = story.name
+        notification.informativeText = "The story's ID has been copied to your clipboard"
+        NSUserNotificationCenter.default.deliver(notification)
+    }
+}
